64aad852f26b2ec83c25bff12106644c5e01b79c
applesdom <applesdom@gmail.com>
2024-04-08 15:48:35 -0500

    Add video device discovery

--- src/device.py
+++ src/device.py
@@ -1,13 +1,30 @@
 import subprocess
 
-class Device:
-    def __init__(self):
-        self.name = 'lol'
+class VideoDevice:
+    def __init__(self, name, handle):
+        self.name = name
+        self.handle = handle
 
-def poll_devices():
+    def __str__(self):
+        return '%s (%s)' % (self.name, self.handle)
+
+def poll_video_devices():
+    # Run command to get list of devices
     result = subprocess.run(['v4l2-ctl', '--list-devices'], stdout=subprocess.PIPE)
+
+    # Parse names and handles
+    # TODO: Handle no-devices-connected case
+    result_string = result.stdout.decode('utf-8')
     devices = []
-    for line in result.stdout.decode('utf-8').split('\n'):
-        if len(line) > 0 and not line.startswith('\t'):
-            devices.append(line[0:line.index(' (')])
+    name = None
+    for line in result_string.split('\n'):
+        if len(line) == 0:
+            continue
+        elif not line.startswith('\t'):
+            name = line[0:line.index(' (')]
+        elif name != None:
+            handle = line[1:]
+            devices.append(VideoDevice(name, handle))
+            name = None
+
     return devices
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 from device import *
 
 def main():
-    for device in poll_devices():
+    for device in poll_video_devices():
         print(device)
 
 if __name__ == '__main__':
