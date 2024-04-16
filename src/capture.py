248d406f82fd5942063d16f2dc92b8df4eb63b24
applesdom <applesdom@gmail.com>
2024-04-16 13:23:28 -0500

    Add Capture class

---
+++ src/capture.py
@@ -0,0 +1,66 @@
+import os
+import threading
+
+from device import *
+
+# Each Capture manages a single child ffmpeg process
+# Set params, then call start(), 
+class Capture:
+    # Status constants
+    INITIALIZED = 0
+    RUNNING     = 1
+    STOPPED     = 2
+    FAULTED     = 3
+
+    def __init__(self):
+        self.child = None
+        self.status = Capture.INITIALIZED
+
+        # Params (no effect after .start() is called)
+        self.video_device = None
+        self.video_format = 'mjpeg'
+        self.video_size = '640x480'
+        self.video_framerate = '60'
+        self.video_bitrate = '1800k'
+        self.video_overlay = True
+        self.video_overlay_template = '%{localtime\:%a, %b %e, %Y %-I\\\\\:%M\\\\\:%S %p}'
+        self.output_dir = './cam1'
+        self.output_template = 'cam1_%Y-%m-%d_%H-%M-%S.mp4'
+        self.output_segment_length = '3600'
+
+    def start(self):
+        # Create output dir, if needed
+        output_dir_absolute = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.output_dir)
+        print(output_dir_absolute)
+        if not os.path.exists(self.output_dir):
+            print('Creating output directory: %s' % (self.output_dir))
+            os.makedirs(self.output_dir, exist_ok=True)
+
+        # Prepare callback
+        def monitor_process(process):
+            print('Started ffmpeg child process (pid=%d)' % (process.pid))
+            line = process.stdout.readline()
+            while len(line) > 0:
+                for subline in line[:-1].decode('utf-8').split('\r'):
+                    print(subline)
+                line = process.stdout.readline()
+            print('ffmpeg child process (pid=%d) has ended' % (process.pid))
+
+        video_args = ['-f', 'v4l2', '-thread_queue_size', '1024', '-input_format', self.video_format, '-s', self.video_size, '-i', self.video_device]
+        if self.video_overlay:
+            overlay_args = ['-vf', 'drawtext=\"fontfile=FreeSans.ttf:fontsize=12:fontcolor=white:x=2:y=2:shadowcolor=black:shadowx=1:shadowy=1:text=\'' + self.video_overlay_template + '\'\"',]
+        else:
+            overlay_args = []
+        format_args = ['-pix_fmt', 'yuv420p', '-c:v', 'h264_v4l2m2m', '-flags', '+global_header', '-b:v', self.video_bitrate, '-y', '-hide_banner']
+        output_args = ['-f', 'segment', '-segment_format_options', 'movflags=+faststart', '-segment_time', self.output_segment_length, '-segment_atclocktime', '1', '-reset_timestamps', '1', '-strftime', '1', self.output_dir + self.output_template]
+        cmd = ['ffmpeg'] + video_args + overlay_args + format_args + output_args
+
+        print('Executing command:')
+        print(' '.join(cmd))
+    
+        self.child = subprocess.Popen(cmd, stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+        threading.Thread(target=monitor_process, args=[self.child]).start()
+
+    def stop(self):
+        self.child.kill()
--- src/main.py
+++ src/main.py
@@ -1,10 +1,27 @@
 #!/bin/python3
 
+import logging
+import time
+
+from capture import *
 from device import *
 
 def main():
-    for device in poll_video_devices():
-        print(device)
+    device_list = poll_video_devices()
+    # print('Found %d devices:')
+    # for device in device_list:
+    #     print('\t' + str(device))
+    device = device_list[0]
+
+    capture = Capture()
+    capture.video_device = device.handle
+    capture.start()
+
+    print(capture.status)
+
+    time.sleep(5)
+
+    capture.stop()
 
 if __name__ == '__main__':
     main()
