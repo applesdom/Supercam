c14c64b3f4953524baf366ca14f2da52e5278eeb
applesdom <applesdom@gmail.com>
2024-04-17 01:27:14 -0500

    Fix log file path not being relative to file dir

--- src/log.py
+++ src/log.py
@@ -1,13 +1,18 @@
 import logging
+import os
 
 LOG_FILE  = './supercam.log'
 LOG_LEVEL = logging.DEBUG
 
 def set_up_logging():
+    # Resolve absolute path of log file
+    log_file = os.path.abspath(os.path.join(os.path.dirname(__file__), LOG_FILE))
+
+    # Create handlers
     formatter = logging.Formatter(fmt='[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S', style='%')
     print_handler = logging.StreamHandler()
     print_handler.setFormatter(formatter)
-    file_handler = logging.FileHandler(LOG_FILE, encoding='utf-8')
+    file_handler = logging.FileHandler(log_file, encoding='utf-8')
     file_handler.setFormatter(formatter)
 
     # Apply to root logger
