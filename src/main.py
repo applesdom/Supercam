6e212c21a6804eae80d11c2f1eab7ac83fa75e6a
applesdom <applesdom@gmail.com>
2024-04-22 00:38:39 -0500

    Add tests to main

--- src/main.py
+++ src/main.py
@@ -9,24 +9,25 @@ from log import *
 
 def main():
     set_up_logging()
-
     logging.info('Supercam has begun.')
 
+    # Device discovery test
     device_list = poll_video_devices()
-    # print('Found %d devices:')
-    # for device in device_list:
-    #     print('\t' + str(device))
-    device = device_list[0]
+    if len(device_list) == 0:
+        logging.info('No devices found.')
+    else:
+        logging.info('Found %d devices:')
+        for device in device_list:
+            logging.info('\t' + str(device))
 
+    # 5 second video capture test
     capture = Capture()
-    capture.video_device = device.handle
+    capture.video_device = '/dev/video0'
+    logging.info('Starting video capture...')
     capture.start()
-
-    print(capture.status)
-
     time.sleep(5)
-
     capture.stop()
+    logging.info('Ended video capture')
 
 if __name__ == '__main__':
     main()
